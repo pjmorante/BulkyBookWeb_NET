5b4cc42e3f44a51d126f4fc557ae920721e2401d
Pedro Morante <pedro.morante@outlook.com>
2023-01-05 22:16:41 -0500

    Add Data Folder and DbContext Class

---
+++ BulkyBook/BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        //public DbSet<CoverType> CoverTypes { get; set; }
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        //public DbSet<Company> Companies { get; set; }
+        //public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        //public DbSet<OrderHeader> OrderHeaders { get; set; }
+        //public DbSet<OrderDetail> OrderDetail { get; set; }
+    }
+}
