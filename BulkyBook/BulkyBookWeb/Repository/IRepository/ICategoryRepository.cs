aa70775d5d47acf116fd4da248f4423878afc09b
Pedro Morante <pedro.morante@outlook.com>
2023-01-09 16:47:36 -0500

    Add IUnitOfWork and UnitOfWork Repository

--- BulkyBook/BulkyBookWeb/Repository/IRepository/ICategoryRepository.cs
+++ BulkyBook/BulkyBookWeb/Repository/IRepository/ICategoryRepository.cs
@@ -5,6 +5,5 @@ namespace BulkyBookWeb.Repository.IRepository
     public interface ICategoryRepository : IRepository<Category>
     {
         void Update(Category obj);
-        void Save();
     }
 }
---
+++ BulkyBook/BulkyBookWeb/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿namespace BulkyBookWeb.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        //ICoverTypeRepository CoverType { get; }
+        //IProductRepository Product { get; }
+        //ICompanyRepository Company { get; }
+        //IShoppingCartRepository ShoppingCart { get; }
+        //IApplicationUserRepository ApplicationUser { get; }
+        //IOrderDetailRepository OrderDetail { get; }
+        //IOrderHeaderRepository OrderHeader { get; }
+
+        void Save();
+    }
+}
